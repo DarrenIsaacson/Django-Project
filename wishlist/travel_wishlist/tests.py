ed1a2d549a8099803dd48acff5fecc129ef73d95
Darren Isaacson <yc8121yl@go.minnstate.edu>
2020-04-07 02:50:45 -0500

    Added some tests and a base html doc from lab

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Place
+
+class TestHomePageIsEmptyList(TestCase):
+
+    def test_load_home_page_shows_empty_list(self):
+        response = self.client.get(reverse('place_list'))
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertFalse(response.context['places'])
+        self.assertContains(response, 'You have no places in your wishlist')
+
+class TestWishList(TestCase):
+    fixtures = ['test_places']
+
+    def test_view_wishlist_contains_not_visited_places(self):
+        response = self.client.get(reverse('place_list'))
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+
+        # self.assertContains(response, 'Canada')
+        # self.assertContains(response, 'France')
+        self.assertNotContains(response, 'Germany')
+        self.assertNotContains(response, 'Dubai')
 
 # Create your tests here.
